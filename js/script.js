5aee2faea0d55c299e867c6846908ebe6400c76c
Polina <polina14.05@yandex.ru>
2017-04-20 22:43:57 +0300

    заменила скорбкт

--- js/script.js
+++ js/script.js
@@ -1,13 +1,15 @@
-var openCart = document.querySelectorAll('.popular-assort__buy');
+var openCart = document.querySelectorAll(".popular-assort__buy");
   //var popupCart = document.querySelector('.popup');//попар
-var countBtn = document.querySelector('.top__item--basket');//стало красным
- var counter = countBtn.querySelector('.number');//цифра
+var countBtn = document.querySelector(".top__item--basket");//стало красным
+ var counter = countBtn.querySelector(".number");//цифра
 
 for (var i = 0; i < openCart.length; i++) {
-    openCart[i].addEventListener('click', function (event) {
+  openCart[i].addEventListener("click", function (event) {
+  openCart[i].addEventListener("click", function (event) {
+    openCart[i].addEventListener("click", function (event) {
         event.preventDefault();
       //  popupCart.classList.add('modal-cart-show');
-        countBtn.classList.add('.top__item--active');
+        countBtn.classList.add(".top__item--active");
         var count = Number(counter.innerHTML);
         counter.innerHTML = count += 1;
     });
